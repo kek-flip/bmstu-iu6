0fd17af2641146c5e2064c4416cf2781593a10e7
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-10-05 00:07:02 +0300

    Добавил функцию добавления товара в таблицу

--- Internet programming languages/LR4/scripts/part1.js
+++ Internet programming languages/LR4/scripts/part1.js
@@ -1,3 +1,28 @@
 function addGoodToTable() {
-    var form = document.getElementById("good-form");
+    if (typeof (addGoodToTable.counter) == 'undefined') {
+        addGoodToTable.counter = 1;
+    }
+
+    var tbody = document.getElementsByTagName('tbody')[0];
+
+    var row = document.createElement('tr');
+
+    var counter = document.createElement('td');
+    counter.textContent = addGoodToTable.counter;
+
+    var goodName = document.createElement('td');
+    goodName.textContent = document.getElementById('good-name').value;
+
+    var goodPrice = document.createElement('td');
+    goodPrice.textContent = document.getElementById('good-price').value;
+
+    row.appendChild(counter);
+    row.appendChild(goodName);
+    row.appendChild(goodPrice);
+
+    tbody.appendChild(row);
+
+    addGoodToTable.counter++;
+    document.getElementById('good-name').value = '';
+    document.getElementById('good-price').value = '';
 }

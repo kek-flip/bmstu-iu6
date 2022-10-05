e1266f4d65d2d9fd5f45bd9e5da44e4a1376bc7b
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-10-05 23:59:22 +0300

    Закончил лабу 4

--- Internet programming languages/LR4/scripts/part1.js
+++ Internet programming languages/LR4/scripts/part1.js
@@ -1,20 +1,22 @@
-function addGoodToTable() {
-    if (typeof (addGoodToTable.counter) == 'undefined') {
-        addGoodToTable.counter = 1;
+function addGoodToOrder(name, price) {
+    if (typeof (addGoodToOrder.counter) == 'undefined') {
+        addGoodToOrder.counter = 1;
     }
 
-    var tbody = document.getElementsByTagName('tbody')[0];
+    var tbody = document.getElementById('order-list');
+    console.log(tbody.nodeName);
 
     var row = document.createElement('tr');
 
     var counter = document.createElement('td');
-    counter.textContent = addGoodToTable.counter;
+    counter.textContent = addGoodToOrder.counter;
 
     var goodName = document.createElement('td');
-    goodName.textContent = document.getElementById('good-name').value;
+    goodName.textContent = name;
 
     var goodPrice = document.createElement('td');
-    goodPrice.textContent = document.getElementById('good-price').value;
+    goodPrice.textContent = price;
+    goodPrice.classList.add('price-col');
 
     row.appendChild(counter);
     row.appendChild(goodName);
@@ -22,7 +24,41 @@ function addGoodToTable() {
 
     tbody.appendChild(row);
 
-    addGoodToTable.counter++;
-    document.getElementById('good-name').value = '';
-    document.getElementById('good-price').value = '';
+    addGoodToOrder.counter++;
+    document.getElementById('order-good-name').value = '';
+    document.getElementById('order-good-price').value = '';
+}
+
+function getData() {
+    var name = document.getElementById('order-good-name').value;
+    var price = document.getElementById('order-good-price').value;
+    addGoodToOrder(name, price);
+}
+
+function caclPrice() {
+    var prices = document.getElementsByClassName('price-col');
+    var sum = 0;
+    for (price of prices) {
+        sum += parseInt(price.textContent);
+    }
+    document.getElementById('sum-text').textContent = 'Итого: ' + sum;
+}
+
+function configPage() {
+    var tbody = document.getElementById('goods-list');
+    console.log(tbody.nodeName);
+    for (var i of tbody.children) {
+        i.classList.add('good-row');
+        i.addEventListener('click', (event) => {
+            var row = event.target.parentElement;
+            addGoodToOrder(row.children[1].textContent, row.children[2].textContent);
+        })
+    }
+}
+
+function clearOrder() {
+    var tbody = document.getElementById('order-list');
+    while(tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
 }

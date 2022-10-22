98f7d06d1b53da5e9b6d5cbafbc245d5532d2b07
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-10-22 14:26:37 +0300

    Защитил пятую лабу

--- Internet programming languages/LR5/part3/tests.rb
+++ Internet programming languages/LR5/part3/tests.rb
@@ -8,6 +8,11 @@ RSpec.describe 'correct_string()' do
     str = (1..rand(1..100)).map { vowels[rand(12)] }.join(' ')
     expect(correct_string(str)).to eq(str)
   end
+  it 'Words only of consonants' do
+    consonants = %w[B b C c D d F f G g H h J j K k L l M m N n P p Q q R r S s T t V v W w X x Z z]
+    str = (1..rand(1..100)).map { consonants[rand(40)] }.join(' ')
+    expect(correct_string(str)).to eq('')
+  end
   it 'Random words' do
     str = (1..rand(1..100)).map { ('a'.ord + rand(26)).chr }.join(' ')
     expect(correct_string(str)).to eq(str.split(' ').select { |w| (w =~ /[AaEeIiOoUuYy]/).nil? == false }.join(' '))
@@ -15,8 +20,14 @@ RSpec.describe 'correct_string()' do
 end
 
 RSpec.describe 'wrong_words_num()' do
-  it 'Random words' do
-    str = (1..rand(1..100)).map { ('a'.ord + rand(26)).chr }.join(' ')
-    expect(wrong_words_num(str)).to eq(str.split(' ').select { |w| (w =~ /[AaEeIiOoUuYy]/).nil? }.length)
+  it 'Words only of consonants' do
+    consonants = %w[B b C c D d F f G g H h J j K k L l M m N n P p Q q R r S s T t V v W w X x Z z]
+    str = (1..rand(1..100)).map { consonants[rand(40)] }.join(' ')
+    expect(wrong_words_num(str)).to eq(str.split(' ').length)
+  end
+  it 'Words only of vowels' do
+    vowels = %w[A a E e I i O o U u Y y]
+    str = (1..rand(1..100)).map { vowels[rand(12)] }.join(' ')
+    expect(wrong_words_num(str)).to eq(0)
   end
 end

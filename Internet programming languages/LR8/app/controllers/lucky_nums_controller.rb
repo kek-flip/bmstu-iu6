386905f251f5cd5f5ac544b2e45536c0cb091f4e
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-12-04 12:33:38 +0300

    Защитил восьмую лабу

---
+++ Internet programming languages/LR8/app/controllers/lucky_nums_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Lucky Nums Controller
+class LuckyNumsController < ApplicationController
+  before_action :validate_num, only: :view
+
+  def input; end
+
+  def view
+    @n = params[:num].to_i
+    @lucky_nums = {}
+    0.upto(@n) do |i|
+      i_str = i.to_s
+      i_str.insert(0, '0' * (6 - i_str.length))
+      @lucky_nums[i] = helpers.calc_sum(i_str, 0, 3) if helpers.calc_sum(i_str, 0, 3) == helpers.calc_sum(i_str, 3, 6)
+    end
+  end
+
+  private
+  
+  def validate_num
+    redirect_to(root_path, alert: "Ошибка! Число должно быть > 0") if params[:num].to_i < 0
+  end
+end
---
+++ Internet programming languages/LR8/spec/requests/lucky_nums_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "LuckyNums", type: :request do
+  describe "GET /input" do
+    it "returns http success" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /view" do
+    it "returns http success" do
+      get lucky_nums_path
+      expect(response).to have_http_status(:success)
+    end
+    it "returns http 302 with wrong params" do
+      get lucky_nums_path, params: {num: -12}
+      expect(response).to have_http_status(302)
+    end
+  end
+end
